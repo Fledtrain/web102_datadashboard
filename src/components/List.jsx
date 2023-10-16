8904b853e956ceb47f5cb9f5dce3ef13117d5ff4
Fledtrain <Joeyew2014@gmail.com>
2023-10-15 23:45:04 -0400

    Finished List.jsx added Table and added form to update fetch request

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,20 +1,52 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 
+const API_KEY = import.meta.env.VITE_API_KEY
 // List - a box container that stores a continuous group of information
 const List = () => {
+    const [form, setForm] = useState([])
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    const [form, setForm] = useState("")
+    // For parameters
+    const [city, setCity] = useState("Raleigh");
+    const [units, setUnits] = useState("M")
+
+    const [temp, setTemp] = useState(0);
+    // Converting Celsius to Fahrenheit (C × 9/5) + 32 = F
+
+    // console.log(data !== "")
+    // console.log(data)
+    // console.log(data?.city_name)
+    // console.log(data?.app_temp)
+    // console.log(data?.uv)
+    // console.log(data?.weather.description)
+    if (data) { console.log(JSON.stringify(data.city_name)) }
+    // console.log(Object.values(data)?.map((item) => {
+    //     console.log(item.city_name, item.app_temp, item.uv, item.weather.description);
+    // }));
 
     const search = (e) => {
         e.preventDefault()
-        setForm(e.target.value)
+        setCity(e.target.value)
     }
-
     const submit = (e) => {
         e.preventDefault()
-        console.log(form)
+        setLoading(true)
+        setForm([city, units])
     }
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            const res = await fetch(`https://api.weatherbit.io/v2.0/current?&city=${form[0]}&units=${form[1]}&key=${API_KEY}`)
+            const data = await res.json()
+            setData(data.data[0])
+            setLoading(false)
+        }
+
+        // fetchWeather()
+    }, [form])
+
     return (
         <>
             <section className="Temp">
@@ -29,14 +61,53 @@ const List = () => {
                     <form>
                         <input
                             type="text"
-                            placeholder="Search..."
+                            placeholder="Enter City..."
                             className="input input-bordered w-full max-w-xs mr-2"
                             onChange={(e) => search(e)} />
-                        <button
-                            className="btn "
-                            onClick={submit}>Search</button>
+                        <select
+                            className="select select-bordered w-full max-w-xs mr-2"
+                            onChange={(e) => setUnits(e.target.value)}>
+                            <option value="DEFAULT" disabled selected>What Unit Temperature?</option>
+                            <option value="M">Metric</option>
+                            <option value="S">Scientific</option>
+                            <option value="I">Fahrenheit</option>
+                        </select>
+                        {loading ?
+                            <p
+                                className="ml-6 loading loading-spinner text-info"
+                            >
+                            </p> :
+                            <button
+                                className="btn" onClick={submit}
+                            >Search.</button>}
                     </form>
-                    <div>{form}</div>
+                    <div>
+                        <table className="table mt-5">
+                            <thead className="bg-neutral text-white text-center" >
+                                <tr>
+                                    <th className="">Station⌛</th>
+                                    <th className="">Feels like Temperature🔥</th>
+                                    <th className="">Elevation Angle💨</th>
+                                    <th className="">Latitude🌌</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data ?
+                                    <tr className="text-center">
+                                        <td className="">{data?.station}</td>
+                                        <td className="">{data?.app_temp} {units} unit</td>
+                                        <td className="">{data?.uv}</td>
+                                        <td className="">{data?.weather.description}</td>
+                                    </tr> : <tr className="text-center">
+                                        <td className="">Fill</td>
+                                        <td className="">In {units} unit</td>
+                                        <td className="">Data </td>
+                                        <td className="">Above</td>
+                                    </tr>
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </section>
             </section >
         </>
