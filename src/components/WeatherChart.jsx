157c1977d08f287e66da9c693fa5993dc2671dfe
Fledtrain <Joeyew2014@gmail.com>
2023-10-22 18:58:39 -0400

    Removed chart from prod

--- src/components/WeatherChart.jsx
+++ src/components/WeatherChart.jsx
@@ -9,7 +9,7 @@ const WeatherChart = ({ weeklyData }) => {
     return (
         <>
             <div>WeatherChart</div>
-            <LineChart width={730} height={250} data={data}
+            {/* <LineChart width={730} height={250} data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
@@ -18,7 +18,7 @@ const WeatherChart = ({ weeklyData }) => {
                 <Legend />
                 <Line type="monotone" dataKey="temp" stroke="#8884d8" />
                 <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+            </LineChart> */}
         </>
     )
 }
